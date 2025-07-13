3c0ed9ba4995cc15ed5794c53323e009103a0371
haku09128910 <h0912-8910@au.com>
2025-07-13 19:18:32 +0900

    開発完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "2222"
+    end
+  end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
